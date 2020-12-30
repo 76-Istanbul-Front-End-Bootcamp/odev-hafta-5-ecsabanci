850bcb391d5fc432064df9ba9e7103c512b6e20e
Emre Sabancı <ecsabancii@gmail.com>
2020-12-30 16:21:36 +0000

    Ödev Tamamlandı.(1)

--- odev.js
+++ odev.js
@@ -33,4 +33,91 @@ document.querySelector("#reset").addEventListener("click", () => {
 
 /* START CODING HERE */
 
+// Select City
 
+const customSelect = document.querySelector(".custom-select");
+
+data.forEach((value,index) => {
+  const option = document.createElement("option");
+  option.setAttribute("value",index);
+
+  option.innerHTML = value.name;
+
+  customSelect.appendChild(option);
+
+})
+
+
+
+customSelect.addEventListener('change', (event) => {
+
+  let selectedCity = Number(event.target.value);
+  
+  createTableElements([data[selectedCity]], "singlecity");
+  
+
+});
+
+
+// Population - bigger than 500.000
+
+const populationBigger = document.querySelector("#populationBigger");
+
+
+populationBigger.addEventListener("click", function(){
+
+  const biggerThan = data.filter(city => {
+    return city.population > 500000;
+  })
+
+  createTableElements(biggerThan, "allcities");
+
+  
+});
+
+// land area - less than 1000
+
+const landArea = document.querySelector("#landAreaLess");
+
+landArea.addEventListener("click", function(){
+
+  const lessThan = data.filter(city => {
+    return city.landArea < 1000;
+  })
+
+  createTableElements(lessThan, "allcities");
+
+  
+})
+
+// does any city has population less than 100.000?
+
+const isPopulationLess = document.querySelector("#isPopulationLess");
+
+isPopulationLess.addEventListener("click", function(){
+
+  const isLess = data.filter(city => {
+    return city.population < 100000;
+  })
+
+  if(isLess.length > 0){
+    toastr.info(`There is ${isLess.length} city/cities Population less than 100.000.`)
+  }
+})
+
+
+// does every city has land area bigger than 100?
+
+const everyLandArea = document.querySelector("#isLandBigger");
+
+everyLandArea.addEventListener("click", function(){
+
+  const everyLand = data.every(city => {
+    return city.landArea > 100;
+  })
+
+  if(everyLand){
+    toastr.info(`Yes, every city has land area bigger than 100`)
+  }
+
+})
